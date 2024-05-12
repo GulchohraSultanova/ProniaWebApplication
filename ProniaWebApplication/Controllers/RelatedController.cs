83224c931d4237b841d32e031b351a577321445e
Gulcohra18 <gulcohrsultanov@gmail.com>
2024-05-12 16:06:50 +0400

    RegisterPageForWebApplication

--- ProniaWebApplication/Controllers/RelatedController.cs
+++ ProniaWebApplication/Controllers/RelatedController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+using ProniaWebApplication.DTOs.AcountDto;
+using ProniaWebApplication.Models;
 
 namespace ProniaWebApplication.Controllers
 {
     public class RelatedController : Controller
     {
+        readonly UserManager<User> _userManager;
+        readonly SignInManager<User> _signInManager;   
+
+        public RelatedController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Account()
         {
             return View();
@@ -11,6 +24,36 @@ namespace ProniaWebApplication.Controllers
         public IActionResult LoginRegister()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LoginRegister(RegisterDto registerDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            User user = new User()
+            {
+                Name = registerDto.Name,
+                Surname = registerDto.Surname,
+                Email = registerDto.Email,
+                UserName=registerDto.Username
+            };
+             var result= await _userManager.CreateAsync(user,registerDto.Password);
+            if(!result.Succeeded) {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View();
+            }
+            await _signInManager.SignInAsync(user, false);
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
         public IActionResult Cart()
         {
--- ProniaWebApplication/Program.cs
+++ ProniaWebApplication/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProniaWebApplication.DAL;
+using ProniaWebApplication.Models;
 
 namespace ProniaWebApplication
 {
@@ -11,20 +13,28 @@ namespace ProniaWebApplication
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddIdentity<User, IdentityRole>(x =>
+            {
+                x.Password.RequiredLength = 8;
+                x.User.RequireUniqueEmail = true;
+                x.User.AllowedUserNameCharacters= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._";
+                x.Lockout.MaxFailedAccessAttempts = 3;
+            }).AddEntityFrameworkStores<AppDbContext>();
             builder.Services.AddDbContext<AppDbContext>(opt =>
             {
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
             });
-            
+
 
             var app = builder.Build();
 
-           
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
             app.MapControllerRoute(
           name: "areas",
           pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
@@ -33,7 +43,7 @@ namespace ProniaWebApplication
             app.MapControllerRoute(
                   name: "default",
                   pattern: "{controller=Home}/{action=Index}/{id?}");
-          
+
 
 
             app.Run();
